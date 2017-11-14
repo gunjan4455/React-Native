aa5652c9e4a14592aaec8205f2c4d68682093ccd
gunjan4455 <gunjan4455@gmail.com>
2017-11-14 12:07:32 +0530

    file added

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import RootNavigator from './src/js/modules/app/rootNavigator'
+import Loader from './src/js/modules/loader'
+
+export default class App extends Component {
+  render() {
+   
+    return (
+      <View style={{ flex: 1 }} >
+        <RootNavigator/>
+      
+      </View>
+    );
+  }
+}
